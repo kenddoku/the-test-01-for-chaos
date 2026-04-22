285d5ba7861a4d2106dd0b4f26434b578590adfd
kenddoku <krzmedrek@gmail.com>
2026-04-22 22:54:24 +0200

    Added ProjectPaths header to store important paths

---
+++ include/ProjectPaths.h
@@ -0,0 +1,14 @@
+#ifndef PROJECTPATHS_H
+#define PROJECTPATHS_H
+
+#include <string>
+
+namespace ProjectPaths {
+    const std::string project_path = "/net/pr2/projects/plgrid/plgghopfieldmgr/Masters";
+
+    const std::string data_path = project_path + "/data";
+
+    const std::string chaos01_out_path = project_path + "/data/chaos01";
+}
+
+#endif
